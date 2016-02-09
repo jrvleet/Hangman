fb716b9e2799899fcd1c1fb7da024bf67c65e80d
Jessica Vliet <jvliet@gmail.com>
2016-02-09 14:13:24 -0800

    fix merge conflicts

--- code/js/data.js
+++ code/js/data.js
@@ -10,44 +10,44 @@ var randomWord;
 var images = {
 	'AV-1': {
 		gallows: [
-		'jpegs/Gallows/gallows_update.png', 'jpegs/Gallows/gallowsWitch_1.png',
-		'jpegs/Gallows/gallowsWitch_2.png', 'jpegs/Gallows/gallowsWitch_3.png', 
-		'jpegs/Gallows/gallowsWitch_4.png', 'jpegs/Gallows/gallowsWitch_5.png',
-		'jpegs/Gallows/gallowsWitch_6.png'
+		'code/jpegs/Gallows/gallows_update.png', 'code/jpegs/Gallows/gallowsWitch_1.png',
+		'code/jpegs/Gallows/gallowsWitch_2.png', 'code/jpegs/Gallows/gallowsWitch_3.png', 
+		'code/jpegs/Gallows/gallowsWitch_4.png', 'code/jpegs/Gallows/gallowsWitch_5.png',
+		'code/jpegs/Gallows/gallowsWitch_6.png'
 		],
 		avatar: [
-		'jpegs/Witch/witchWhole_1.png', 'jpegs/Witch/witch_2.png',
-		'jpegs/Witch/witch_3.png', 'jpegs/Witch/witch_4.png',
-		'jpegs/Witch/witch_5.png', 'jpegs/Witch/witch_6.png',
-		'jpegs/Skeleton/skel_blank.png'
+		'code/jpegs/Witch/witchWhole_1.png', 'code/jpegs/Witch/witch_2.png',
+		'code/jpegs/Witch/witch_3.png', 'code/jpegs/Witch/witch_4.png',
+		'code/jpegs/Witch/witch_5.png', 'code/jpegs/Witch/witch_6.png',
+		'code/jpegs/Skeleton/skel_blank.png'
 		]
 	},
 	'AV-2': {
 		gallows: [
-		'jpegs/Gallows/gallows_update.png', 'jpegs/Gallows/gallowsDevil_1.png',
-		'jpegs/Gallows/gallowsDevil_2.png', 'jpegs/Gallows/gallowsDevil_3.png', 
-		'jpegs/Gallows/gallowsDevil_4.png', 'jpegs/Gallows/gallowsDevil_5.png',
-		'jpegs/Gallows/gallowsDevil_6.png'
+		'code/jpegs/Gallows/gallows_update.png', 'code/jpegs/Gallows/gallowsDevil_1.png',
+		'code/jpegs/Gallows/gallowsDevil_2.png', 'code/jpegs/Gallows/gallowsDevil_3.png', 
+		'code/jpegs/Gallows/gallowsDevil_4.png', 'code/jpegs/Gallows/gallowsDevil_5.png',
+		'code/jpegs/Gallows/gallowsDevil_6.png'
 		],
 		avatar: [
-		'jpegs/Devil/devilWhole_1.png', 'jpegs/Devil/devil_2.png',
-		'jpegs/Devil/devil_3.png', 'jpegs/Devil/devil_4.png',
-		'jpegs/Devil/devil_5.png', 'jpegs/Devil/devil_6.png',
-		'jpegs/Skeleton/skel_blank.png'
+		'code/jpegs/Devil/devilWhole_1.png', 'code/jpegs/Devil/devil_2.png',
+		'code/jpegs/Devil/devil_3.png', 'code/jpegs/Devil/devil_4.png',
+		'code/jpegs/Devil/devil_5.png', 'code/jpegs/Devil/devil_6.png',
+		'code/jpegs/Skeleton/skel_blank.png'
 		]
 	},
 	'AV-3': {
 		gallows: [
-		'jpegs/Gallows/gallows_update.png', 'jpegs/Gallows/gallowsSkel_1.png',
-		'jpegs/Gallows/gallowsSkel_2.png', 'jpegs/Gallows/gallowsSkel_3.png', 
-		'jpegs/Gallows/gallowsSkel_4.png', 'jpegs/Gallows/gallowsSkel_5.png',
-		'jpegs/Gallows/gallowsSkel_6.png'
+		'code/jpegs/Gallows/gallows_update.png', 'code/jpegs/Gallows/gallowsSkel_1.png',
+		'code/jpegs/Gallows/gallowsSkel_2.png', 'code/jpegs/Gallows/gallowsSkel_3.png', 
+		'code/jpegs/Gallows/gallowsSkel_4.png', 'code/jpegs/Gallows/gallowsSkel_5.png',
+		'code/jpegs/Gallows/gallowsSkel_6.png'
 		],
 		avatar: [
-		'jpegs/Skeleton/skelWhole_1.png', 'jpegs/Skeleton/skel_2.png',
-		'jpegs/Skeleton/skel_3.5.png', 'jpegs/Skeleton/skel_4.5.png',
-		'jpegs/Skeleton/skel_5.5.png', 'jpegs/Skeleton/skel_6.5.png',
-		'jpegs/Skeleton/skel_blank.png'
+		'code/jpegs/Skeleton/skelWhole_1.png', 'code/jpegs/Skeleton/skel_2.png',
+		'code/jpegs/Skeleton/skel_3.5.png', 'code/jpegs/Skeleton/skel_4.5.png',
+		'code/jpegs/Skeleton/skel_5.5.png', 'code/jpegs/Skeleton/skel_6.5.png',
+		'code/jpegs/Skeleton/skel_blank.png'
 		]
 	}
 };
@@ -63,13 +63,6 @@ $('.avatar').click(function(event){
 	blanksFromAnswer();
 
 	hangman();
-		// if(chosenPlayer.attr("id")==="AV-1") {
-		// 	chooseCharacter === "witch";
-		// } else if (chosenPlayer.attr("id")==="AV-2") {
-		// 	chooseCharacter === "devil";
-		// } else if (chosenPlayer.attr("id")==="AV-3") {
-		// 	chooseCharacter === "skeleton";
-		// }
 	});
 
 var wordBank = ["halloween", "ghost", "spectre", "witch", "candycorn", 
@@ -98,8 +91,6 @@ var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
 var displayButtons = function() {
 	var letter;
 	for(var i = 0; i < alphabet.length; i++) {
-		//letter = $('div').addClass('letters');
-		//letter.html(alphabet[i]);
 		letter = document.createElement('div');
 		letter.innerHTML = alphabet[i];
 		letter.className = 'letters';
@@ -134,7 +125,7 @@ function hangman() {
 	$('#gallows').attr("src", images[chosenPlayer].gallows[countLoses]);
 	$('#character').attr("src", images[chosenPlayer].avatar[countLoses]);
 	if (countLoses === 6) {
-		lose();	
+		loseGame();	
 	} else if (numCorrect === randomWord.length) {
 		winGame();
 		//handle winner
@@ -143,10 +134,14 @@ function hangman() {
 	}
 } 
 
-function lose(){
-	$('#gallows').addClass('animated fadeOut');
-	console.log('loser');
-};
+function loseGame(){
+	$('#loser').removeClass('.hidden').fadeIn(1000);
+	$('#playAgain').removeClass('.hidden').fadeIn(1000);
+	$('.rightColumn').hide();
+	$('.leftColumn').hide();
+	$('#guessWord').hide();
+	$('#buttons').hide();
+}
 
 function winGame() {
 	$('#winner').removeClass('.hidden').fadeIn(1000);
